4abfb00a5c4ef2cdda053bf4c67cae1c43090f1b
Krishnaveni2025 <krishnavenipurne@gmail.com>
2025-03-19 18:52:12 +0530

    Add files via upload

---
+++ Day22_C25022025.c
@@ -0,0 +1,22 @@
+/*C PROGRAMMING SELF ASSESSMENT 10
+Don't use Stuctures*/
+/*1. Get an alphabet and print is ASCII value. 
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter an alphabet: ");
+    scanf("%c", &ch);
+    printf("The ASCII value of %c is %d\n", ch, ch);
+    return 0;
+}*/
+/*2. Get a single number and print its ASCII value. */
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("The ASCII value of %d is %c\n", num, num);
+    return 0;
+}
